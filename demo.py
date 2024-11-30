04c33fdb8af53d17d2d6193076be2f4eda7097fa
Le Giang <hoanglegiang9tc105@gmail.com>
2024-11-30 23:33:53 +0700

    add code

--- demo.py
+++ demo.py
@@ -7,6 +7,10 @@ print(new_string)  # Output: edcba
 print("Nhap so tu nhien: ")
 n = int(input())
 if(n%2==0):
-    print("So ", n , "la so chan")
+    print("So", n , "la so chan")
 else:
-    print("So ", n , "la so le")
+    print("So", n , "la so le")
+
+my_string = "Hoang Le Giang"
+new_string = my_string.split()
+print(new_string)
